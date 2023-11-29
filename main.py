6dbdfa8e5fc719b7b599a9d26f19c8b09661acb8
stkvet2020 <67281402+stkvet2020@users.noreply.github.com>
2023-11-29 06:57:11 -0600

    First commit , blank files.

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is VMIS: Veterinary Management Information System
+# Written by Stephan Knappstein
+# Project initiated on Nov 29th , 2023
+
+
+
+def main():
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
